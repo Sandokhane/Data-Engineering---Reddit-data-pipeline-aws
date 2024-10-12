735f49b284b9f19fb00d65a4a1b967d5e680383e
Highashikata <bassambenidir@yahoo.com>
2024-10-12 23:43:19 +0200

    Alors réécriture du script des python des DAGs ainsi que le script du upload to S3

---
+++ utils/s3_upload.py
@@ -0,0 +1,68 @@
+import boto3
+import os
+import csv
+import praw
+from dotenv import load_dotenv
+import logging
+
+# Initialiser les Logs
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Charger les credentials AWS depuis le fichier .env
+# AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_BUCKET_NAME = os.getenv('S3_BUCKET_RAW')
+
+# Credentials Reddit
+# REDDIT_CLIENT_ID = os.getenv('REDDIT_CLIENT_ID')
+# REDDIT_SECRET = os.getenv('REDDIT_CLIENT_SECRET')
+# REDDIT_USER_AGENT = os.getenv('REDDIT_USER_AGENT')
+
+
+# Fonction pour uploader un fichier sur S3
+def upload_to_s3(file_name, bucket, object_name=None):
+    """Upload un fichier sur S3.
+
+    :param file_name: Chemin du fichier local à uploader
+    :param bucket: Nom du bucket S3
+    :param object_name: Nom de l'objet S3. Si non défini, utilise le nom du fichier local
+    """
+    # Si aucun nom d'objet n'est fourni, utiliser le nom du fichier
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Initialiser la connexion S3
+    s3_client = boto3.client('s3')
+
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+        print(f"Fichier {file_name} uploadé avec succès dans {bucket}/{object_name}")
+    except boto3.exceptions.S3UploadFailedError as e:
+        logger.error(f"Ereur lors de l'upload vers S3: {e}")
+    except Exception as e:
+        logger.error(f"Erreur inattendue : {e}")
+
+
+
+
+if __name__ == "__main__":
+    csv_file = os.path.join(os.getcwd(), 'data/muay_thai_reddit_posts_raw.csv')
+
+    # Nom du Bucket
+    bucket_name = AWS_BUCKET_NAME
+
+
+    # Construire le chemin dans S3 sous un dossier "raw"
+    # Construire le chemin dans S3 sous le dossier "raw_data"
+    s3_path = f"raw_data/{os.path.basename(csv_file)}"
+
+    # Uploader le fichier dans S3 sous le chemin raw_data/
+    upload_to_s3(csv_file, bucket_name, s3_path)
+
+   
+
